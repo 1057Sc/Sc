3590449a2aa2035f33525866c004c45530689bbc
qingxin.zhu <17611251996@163.com>
2018-12-21 14:27:35 +0800

    lambda and genericity

--- src/org.sc/Lambda/Demo1.java
+++ src/org.sc/Lambda/Demo1.java
@@ -7,9 +7,10 @@ public class Demo1 {
     final static String salutation = "Hello! ";
 
     public static void main(String args[]){
+        //直接实现一个接口
         GreetingService greetService1 = message ->
-                System.out.println(message);
-        greetService1.sayMessage("Runoob");
+                System.out.println(message);//这里为实现逻辑
+        greetService1.sayMessage("Runoob");//这里为逻辑调用
 
         SayHello sayHello = sayHeelo ->
                 System.out.println(sayHeelo);
---
+++ src/org.sc/genericity/Demo1.java
@@ -0,0 +1,40 @@
+package genericity;
+
+import java.util.Map;
+
+/**
+ * Created by Sc on 2018/12/21.
+ */
+public class Demo1<R,C extends Child> extends Root<C>{
+
+    protected R perform(R r){
+
+        return r;
+    }
+
+    public static void main(String args[]){
+        Demo1 d = new Demo1();
+        Child1 child1 = new Child1<Child1>();
+    }
+
+}
+
+class Root<C>{
+
+}
+
+class Child<C>{
+
+}
+
+class Child1<C> extends Child<C>{
+
+}
+
+class Restrict<List>{
+
+    public Restrict() {
+    }
+
+
+}
