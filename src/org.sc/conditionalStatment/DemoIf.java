c043d5001b515572779265012b57ef386347d636
sc1057 <13525233384@163.com>
2019-05-06 17:06:38 +0800

    daily practice

---
+++ src/org.sc/conditionalStatment/DemoIf.java
@@ -0,0 +1,31 @@
+package conditionalStatment;
+
+/**
+ * Created by duizhuang on 5/5/2019.
+ */
+public class DemoIf {
+
+    public static void main(String[] args) {
+
+        Demo1();
+    }
+
+    public static void Demo1(){
+
+        String s = "111";
+
+        for(int i=0;i<10;i++){
+            if(s.equals("111")){
+                System.out.println("111111111111111111111111");
+                break;
+            }
+
+            if(s.equals("111")){
+                System.out.println(1111);;
+            }
+        }
+
+
+
+    }
+}
