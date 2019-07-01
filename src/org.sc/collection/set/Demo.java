42c9b28d69f33942cfa4c8b34e893c54d7bb9cc9
sc1057 <13525233384@163.com>
2019-07-01 09:02:52 +0800

    collections practice

---
+++ src/org.sc/collection/set/Demo.java
@@ -0,0 +1,37 @@
+package collection.set;
+
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Set;
+
+/**
+ * Created by duizhuang on 6/29/2019.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        demo1();
+        Short s = 1;
+        Integer i = 1;
+        testGenericMethodDefine(s, i);
+    }
+
+    public static void demo1() {
+        Set set = new HashSet();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+        set.add(7);
+
+        System.out.println(set);
+
+
+    }
+
+    public static <T, S extends T> T testGenericMethodDefine(T t, S s){
+
+        return t;
+    }
+}
---
+++ src/org.sc/operator/Or.java
@@ -0,0 +1,15 @@
+package operator;
+
+/**
+ * Created by duizhuang on 6/28/2019.
+ */
+public class Or {
+
+    public static void main(String[] args) {
+        demo1();
+    }
+
+    public static void demo1() {
+        System.out.println(3|9);
+    }
+}
