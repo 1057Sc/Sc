14cc47beaefef9a1c6e7b9cbea0a49c0fce2e8a0
1057Sc <13525233384@163.com>
2019-07-09 21:50:47 +0800

    es-practice

---
+++ src/org.sc/elastic/search/Demo1.java
@@ -0,0 +1,12 @@
+package elastic.search;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void demo1() {
+        IndexRequest request = new IndexRequest("posts");
+    }
+}
