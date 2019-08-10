df33ba1374648c01dc621de2df39f6040e6b6998
sc1057 <13525233384@163.com>
2019-08-10 09:09:20 +0800

    operator

---
+++ src/org.sc/conditionalStatment/WhileDemo.java
@@ -0,0 +1,35 @@
+package conditionalStatment;
+
+/**
+ * Created by duizhuang on 8/10/2019.
+ */
+public class WhileDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(toHexString(1));
+
+        System.out.println(10 & 20);
+
+        System.out.println(4 & 0xF);
+
+        System.out.println(4 | 0xF);
+
+        System.out.println(1 << 4);
+        System.out.println(10 >> 3);
+
+        System.out.println(-1 >>> 2);
+
+    }
+
+
+    public static String toHexString(int i) {
+        StringBuffer buf = new StringBuffer(8);
+        do {
+            buf.append(Character.forDigit(i & 0xF, 16));
+            i >>>= 4;
+        } while (i != 0);
+        return buf.reverse().toString();
+    }
+
+}
