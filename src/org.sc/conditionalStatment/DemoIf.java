cee2b062d53bc97cd8009b60595d859cfe52937f
sc1057 <13525233384@163.com>
2019-08-20 16:49:12 +0800

    practice

--- src/org.sc/conditionalStatment/DemoIf.java
+++ src/org.sc/conditionalStatment/DemoIf.java
@@ -1,5 +1,7 @@
 package conditionalStatment;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * Created by duizhuang on 5/5/2019.
  */
@@ -7,7 +9,8 @@ public class DemoIf {
 
     public static void main(String[] args) {
 
-        Demo1();
+        //Demo1();
+        demo2();
     }
 
     public static void Demo1(){
@@ -25,7 +28,73 @@ public class DemoIf {
             }
         }
 
+    }
 
 
+    public static void demo2(){
+
+        School school = new School();
+        school.setSchoolName("77");
+        school.setSchoolNo("11");
+
+        if (StringUtils.equals(school.getStudent().getName(),"777")) {
+            System.out.println(777777);
+        }
+    }
+}
+
+
+class School{
+    private String schoolName;
+
+    private String schoolNo;
+
+    private Student student;
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getSchoolNo() {
+        return schoolNo;
+    }
+
+    public void setSchoolNo(String schoolNo) {
+        this.schoolNo = schoolNo;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
+
+class Student{
+    private Integer age;
+
+    private String name;
+
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
