dc274e25d45130246a7f85f45d7318d210d380db
sc1057 <13525233384@163.com>
2019-09-02 09:07:16 +0800

    hbase

---
+++ hbabse/src/main/java/org/sc/HbaseClientExample.java
@@ -0,0 +1,41 @@
+package org.sc;
+
+import com.google.protobuf.ServiceException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+import java.io.IOException;
+
+/**
+ * Created by duizhuang on 8/30/2019.
+ */
+public class HbaseClientExample {
+
+    public static void main(String[] args) throws IOException, ServiceException {
+        new HbaseClientExample().connect();
+    }
+
+    private void connect() throws IOException, ServiceException {
+        Configuration config = HBaseConfiguration.create();
+
+        String path = this.getClass().getClassLoader().getResource("hbase-site.xml").getPath();
+        config.addResource(new Path(path));
+
+
+        //config.set("hbase.master", "10.0.100.235:60000");
+        //config.addResource(new Path("/usr/local/hbase/hbase/conf/hbase-site.xml"));
+
+        try {
+            HBaseAdmin.checkHBaseAvailable(config);
+        } catch (MasterNotRunningException e) {
+            System.out.println("HBase is not running." + e.getMessage());
+            return;
+        }
+
+        HBaseClientOperations HBaseClientOperations = new HBaseClientOperations();
+        HBaseClientOperations.run(config);
+    }
+}
