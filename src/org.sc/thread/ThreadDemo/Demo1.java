3b4cd626080e3fdba183b1181116491344b585cc
1057Sc <13525233384@163.com>
2019-09-25 23:21:53 +0800

    practice daily

---
+++ src/org.sc/thread/ThreadDemo/Demo1.java
@@ -0,0 +1,14 @@
+package thread.ThreadDemo;
+
+import java.util.concurrent.locks.Lock;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void dmeo1 (){
+         int i = 1;
+    }
+}
