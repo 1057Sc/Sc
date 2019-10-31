532ac71f68aa9b612e1ebb10768bb8904db33766
sc1057 <13525233384@163.com>
2019-10-31 17:34:01 +0800

    hashMap annotaion

--- src/org.sc/collection/map/MapApi.java
+++ src/org.sc/collection/map/MapApi.java
@@ -11,6 +11,8 @@ public class MapApi {
     public void hcode() {
         int h;
         int i = (h = "key".hashCode()) ^ (h >>> 16);
+        // (p = tab[i = (n - 1) & hash]) 这边作用的应该是为了减少hash碰撞 毕竟初始的table size没有那么大
+
         System.out.println(i);
         HashMap map = new HashMap();
         map.put("key","keyone");
