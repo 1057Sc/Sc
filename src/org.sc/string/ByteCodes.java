4e1bd9a6404a5c4177b42379559809039c026ce8
sc1057 <13525233384@163.com>
2019-11-01 17:06:53 +0800

    character

--- src/org.sc/string/ByteCodes.java
+++ src/org.sc/string/ByteCodes.java
@@ -1,6 +1,9 @@
 package string;
 
+import org.junit.Test;
+
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 /**
@@ -11,5 +14,14 @@ public class ByteCodes {
     public static void main(String[] args) throws UnsupportedEncodingException {
         byte[] b_utf8 = "深".getBytes("UTF-8");
         System.out.println(Arrays.toString(b_utf8));
+    }
+
+    @Test
+    public void demo1(){
+        String s = "汪";
+        System.out.println(s);
+
+        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        System.out.println(new String(bytes, StandardCharsets.UTF_8));
     }
 }
