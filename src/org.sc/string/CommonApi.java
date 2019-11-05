bd8b8e5bd5fc2475445656fe1677ef1a8c55b275
sc1057 <13525233384@163.com>
2019-11-05 17:49:01 +0800

    special circumstances

--- src/org.sc/string/CommonApi.java
+++ src/org.sc/string/CommonApi.java
@@ -12,4 +12,13 @@ public class CommonApi {
         String cp2 = "{\"signatureMethod\":\"HMACSHA1\",\"version\":\"1.0.0\",\"signatureVersion\":\"1.0\",\"timestamp\":1572059884074,\"signatureNonce\":\"k26zv5t60.pzh7eu761yr\"}";
         System.out.println(cp2.toCharArray());
     }
+
+    @Test
+    public void ObjString(){
+        Object s1 = "sajdlajd";
+        System.out.println(s1 instanceof String);
+        System.out.println(s1.toString());
+        System.out.println((String)s1);
+    }
+
 }
