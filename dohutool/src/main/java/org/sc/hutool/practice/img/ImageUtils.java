297f059354d854f89812037ea51b04215e449804
1057Sc <13525233384@163.com>
2019-11-26 21:04:36 +0800

    hutool process image

---
+++ dohutool/src/main/java/org/sc/hutool/practice/img/ImageUtils.java
@@ -0,0 +1,144 @@
+package org.sc.hutool.practice.img;
+
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import static java.lang.reflect.Array.getLength;
+
+public class ImageUtils {
+
+    @Test
+    public void demo1() {
+        // 5-给图片添加文字水印：
+        // 方法一：
+        ImageUtils.pressText("我是水印文字", "e:/abc.jpg", "e:/abc_pressText.jpg", "宋体", Font.BOLD, Color.white, 80, 0, 0, 0.5f);//测试OK
+        // 方法二：
+        ImageUtils.pressText2("我也是水印文字", "e:/abc.jpg", "e:/abc_pressText2.jpg", "黑体", 36, Color.white, 80, 0, 0, 0.5f);//测试OK
+
+        // 6-给图片添加图片水印：
+        ImageUtils.pressImage("e:/abc2.jpg", "e:/abc.jpg", "e:/abc_pressImage.jpg", 0, 0, 0.5f);//测试OK
+    }
+
+    /**
+     * 给图片添加文字水印
+     *
+     * @param pressText     水印文字
+     * @param srcImageFile  源图像地址
+     * @param destImageFile 目标图像地址
+     * @param fontName      水印的字体名称
+     * @param fontStyle     水印的字体样式
+     * @param color         水印的字体颜色
+     * @param fontSize      水印的字体大小
+     * @param x             修正值
+     * @param y             修正值
+     * @param alpha         透明度：alpha 必须是范围 [0.0, 1.0] 之内（包含边界值）的一个浮点数字
+     */
+    public final static void pressText(String pressText,
+                                       String srcImageFile, String destImageFile, String fontName,
+                                       int fontStyle, Color color, int fontSize, int x,
+                                       int y, float alpha) {
+        try {
+            File img = new File(srcImageFile);
+            Image src = ImageIO.read(img);
+            int width = src.getWidth(null);
+            int height = src.getHeight(null);
+            BufferedImage image = new BufferedImage(width, height,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = image.createGraphics();
+            g.drawImage(src, 0, 0, width, height, null);
+            g.setColor(color);
+            g.setFont(new Font(fontName, fontStyle, fontSize));
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,
+                    alpha));
+            // 在指定坐标绘制水印文字
+            g.drawString(pressText, (width - (getLength(pressText) * fontSize))
+                    / 2 + x, (height - fontSize) / 2 + y);
+            g.dispose();
+            ImageIO.write((BufferedImage) image, "JPEG", new File(destImageFile));// 输出到文件流
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 给图片添加文字水印
+     *
+     * @param pressText     水印文字
+     * @param srcImageFile  源图像地址
+     * @param destImageFile 目标图像地址
+     * @param fontName      字体名称
+     * @param fontStyle     字体样式
+     * @param color         字体颜色
+     * @param fontSize      字体大小
+     * @param x             修正值
+     * @param y             修正值
+     * @param alpha         透明度：alpha 必须是范围 [0.0, 1.0] 之内（包含边界值）的一个浮点数字
+     */
+    public final static void pressText2(String pressText, String srcImageFile, String destImageFile,
+                                        String fontName, int fontStyle, Color color, int fontSize, int x,
+                                        int y, float alpha) {
+        try {
+            File img = new File(srcImageFile);
+            Image src = ImageIO.read(img);
+            int width = src.getWidth(null);
+            int height = src.getHeight(null);
+            BufferedImage image = new BufferedImage(width, height,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = image.createGraphics();
+            g.drawImage(src, 0, 0, width, height, null);
+            g.setColor(color);
+            g.setFont(new Font(fontName, fontStyle, fontSize));
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,
+                    alpha));
+            // 在指定坐标绘制水印文字
+            g.drawString(pressText, (width - (getLength(pressText) * fontSize))
+                    / 2 + x, (height - fontSize) / 2 + y);
+            g.dispose();
+            ImageIO.write((BufferedImage) image, "JPEG", new File(destImageFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 给图片添加图片水印
+     * @param pressImg 水印图片
+     * @param srcImageFile 源图像地址
+     * @param destImageFile 目标图像地址
+     * @param x 修正值。 默认在中间
+     * @param y 修正值。 默认在中间
+     * @param alpha 透明度：alpha 必须是范围 [0.0, 1.0] 之内（包含边界值）的一个浮点数字
+     */
+    public final static void pressImage(String pressImg, String srcImageFile,String destImageFile,
+                                        int x, int y, float alpha) {
+        try {
+            File img = new File(srcImageFile);
+            Image src = ImageIO.read(img);
+            int wideth = src.getWidth(null);
+            int height = src.getHeight(null);
+            BufferedImage image = new BufferedImage(wideth, height,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = image.createGraphics();
+            g.drawImage(src, 0, 0, wideth, height, null);
+            // 水印文件
+            Image src_biao = ImageIO.read(new File(pressImg));
+            int wideth_biao = src_biao.getWidth(null);
+            int height_biao = src_biao.getHeight(null);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,
+                    alpha));
+            g.drawImage(src_biao, (wideth - wideth_biao) / 2,
+                    (height - height_biao) / 2, wideth_biao, height_biao, null);
+            // 水印文件结束
+            g.dispose();
+            ImageIO.write((BufferedImage) image,  "JPEG", new File(destImageFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
