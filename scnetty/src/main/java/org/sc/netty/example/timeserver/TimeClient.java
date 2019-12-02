ece1a4e909decbeab921a8e70cdc7496a6938097
zqx <13525233384@163.com>
2019-12-02 08:35:53 +0800

    add timeDecoder

--- scnetty/src/main/java/org/sc/netty/example/timeserver/TimeClient.java
+++ scnetty/src/main/java/org/sc/netty/example/timeserver/TimeClient.java
@@ -23,7 +23,7 @@ public class TimeClient {
             b.handler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 public void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new TimeClientHandler());
+                    ch.pipeline().addLast(new TimeDecoder(), new TimeClientHandler());
                 }
             });
 
