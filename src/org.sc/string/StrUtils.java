a1e62ca0834025fc3dfb7500a34d77b6b954f935
zqx <13525233384@163.com>
2019-12-04 08:50:21 +0800

    toggle case

---
+++ src/org.sc/string/StrUtils.java
@@ -0,0 +1,21 @@
+package string;
+
+import org.junit.Test;
+
+public class StrUtils {
+
+    /**
+     *首字母大写转换  toggle case
+     * ASCII 码前移，ASCII 中大写字母从65开始，小写字母从97开始
+     */
+    @Test
+    public void demo1(){
+        String s = "demo";
+        char[] chars = s.toCharArray();
+        if (chars[0] >= 'a' && chars[0] <= 'z') {
+            chars[0] = (char) (chars[0] - 32);
+            System.out.println(chars[0]);
+        }
+        System.out.println(new String(chars));
+    }
+}
