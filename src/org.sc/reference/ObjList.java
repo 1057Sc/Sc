cfed05354b69cdd3c8f092d7861677bde9a685ba
zqx <13525233384@163.com>
2019-12-04 10:51:51 +0800

    reference pass

---
+++ src/org.sc/reference/ObjList.java
@@ -0,0 +1,16 @@
+package reference;
+
+import java.util.List;
+
+public class ObjList {
+
+    private List<String> list;
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
--- src/org.sc/reference/ValueRef.java
+++ src/org.sc/reference/ValueRef.java
@@ -1,6 +1,10 @@
 package reference;
 
+import org.junit.Test;
+
+import javax.crypto.spec.OAEPParameterSpec;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Sc on 6/12/2019.
@@ -13,26 +17,25 @@ public class ValueRef {
         Demo1(i,s);
         System.out.println(i);
         System.out.println(s);*/
-        Point pnt1 = new Point(0,0);
-        Point pnt2 = new Point(0,0);
-        System.out.println("X: " + pnt1.x + " Y: " +pnt1.y);
-        System.out.println("X: " + pnt2.x + " Y: " +pnt2.y);
+        Point pnt1 = new Point(0, 0);
+        Point pnt2 = new Point(0, 0);
+        System.out.println("X: " + pnt1.x + " Y: " + pnt1.y);
+        System.out.println("X: " + pnt2.x + " Y: " + pnt2.y);
         System.out.println(" ");
-        tricky(pnt1,pnt2);
+        tricky(pnt1, pnt2);
         System.out.println("X: " + pnt1.x + " Y:" + pnt1.y);
-        System.out.println("X: " + pnt2.x + " Y: " +pnt2.y);
+        System.out.println("X: " + pnt2.x + " Y: " + pnt2.y);
 
     }
 
-    public static void Demo1(Integer i,String s){
-        i+=1;
-        s+="777";
+    public static void Demo1(Integer i, String s) {
+        i += 1;
+        s += "777";
 
     }
 
 
-    public static void tricky(Point arg1, Point arg2)
-    {
+    public static void tricky(Point arg1, Point arg2) {
         arg1.x = 100;
         arg1.y = 100;
        /* Point temp = arg1;
@@ -41,6 +44,19 @@ public class ValueRef {
     }
 
 
+    @Test
+    public void demo1(){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("1");
+        strings.add("1");
+        strings.add("1");
+        strings.add("1");
 
+        ObjList objList = new ObjList();
+        objList.setList(strings);
+        System.out.println(strings.toString());
 
+        objList.getList().add("7");
+        System.out.println(strings.toString());
+    }
 }
