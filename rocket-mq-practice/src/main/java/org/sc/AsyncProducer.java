36f9e426df793454b424201260f2560db6987f02
zqx <13525233384@163.com>
2019-12-12 08:45:30 +0800

    mq command

--- rocket-mq-practice/src/main/java/org/sc/AsyncProducer.java
+++ rocket-mq-practice/src/main/java/org/sc/AsyncProducer.java
@@ -6,8 +6,21 @@ import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
+
+/**
+ *  command
+ *   export NAMESRV_ADDR=localhost:9876
+ *   sh bin/tools.sh org.apache.rocketmq.example.quickstart.Producer
+ *   sh bin/tools.sh org.apache.rocketmq.example.quickstart.Consumer
+ *
+ *   nohup sh bin/mqnamesrv &
+ *
+ *   nohup sh bin/mqbroker -n localhost:9876 &   autoCreateTopicEnable=true
+ */
 public class AsyncProducer {
     public static final String host = "10.0.100.235:9876";
+    public static final String host1 = "10.0.100.235:10911";
+    public static final String host2 = "localhost:10911";
 
     public static void main(String[] args) throws Exception {
         //Instantiate with a producer group name.
@@ -16,6 +29,7 @@ public class AsyncProducer {
         producer.setNamesrvAddr(host);
         //Launch the instance.
         producer.start();
+        // producer.setVipChannelEnabled(true);
         producer.setRetryTimesWhenSendAsyncFailed(0);
         for (int i = 0; i < 100; i++) {
             final int index = i;
