a2454ecd44e2c7d1ca4d9df97a5abd232a6ab385
zqx <13525233384@163.com>
2019-12-24 08:30:54 +0800

    sdadasd

--- src/org.sc/lambda/stream/StreamDemo1.java
+++ src/org.sc/lambda/stream/StreamDemo1.java
@@ -197,4 +197,18 @@ public class StreamDemo1 {
         System.out.println(collect1.toString());
 
     }
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("1",1);
+        map.put("2",2);
+        map.put("3",3);
+        map.put("4",4);
+        map.put("5",5);
+        map.put("6",6);
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+        Set<Map.Entry<String, Integer>> entries1 = map.entrySet();
+        Integer reduce = entries.stream().map(Map.Entry::getValue).reduce(1, (x, y) -> (x * y));
+        System.out.println(reduce);
+    }
 }
