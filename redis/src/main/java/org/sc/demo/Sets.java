254b8dfad2086b61beb9b36817c5c758122e1d46
zqx <13525233384@163.com>
2020-03-20 10:24:13 +0800

    daily

---
+++ redis/src/main/java/org/sc/demo/Sets.java
@@ -0,0 +1,22 @@
+package org.sc.demo;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Set;
+
+public class Sets {
+
+    private static final JedisPool pool = new JedisPool(new JedisPoolConfig(), "10.0.100.9");
+
+
+    @Test
+    public void testSet(){
+        Jedis jedis = pool.getResource();
+        String key = "sad";
+        Set<String> smembers = jedis.smembers(key);
+        System.out.println(smembers.size());
+    }
+}
