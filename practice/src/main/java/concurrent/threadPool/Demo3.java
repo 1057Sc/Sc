23d20a49d5323880fa06de0c254778de814f3550
zqx <13525233384@163.com>
2020-03-20 14:52:39 +0800

    daily

---
+++ practice/src/main/java/concurrent/threadPool/Demo3.java
@@ -0,0 +1,45 @@
+package concurrent.threadPool;
+
+import org.junit.Test;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * asynchronized
+ */
+public class Demo3 {
+
+    public static int _count = 0;
+
+
+    @Test
+    public void test() {
+
+        int count = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(count);
+        for (int i = 0; i < count; i++) {
+            int finalI = i;
+            executorService.submit(() -> {
+                System.out.println(finalI);
+            });
+        }
+    }
+
+
+    @Test
+    public void test1() {
+        int count = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(count);
+        for (int i = 0; i < count; i++) {
+            int finalI = i;
+            executorService.submit(() -> {
+                _count++;
+                System.out.println(finalI);
+            });
+        }
+
+        System.out.println("_count:::::::::::::" + _count);
+    }
+}
