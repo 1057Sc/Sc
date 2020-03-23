bf4dad895cb51b456264a641af9eea24310d3004
zqx <13525233384@163.com>
2020-03-23 08:39:27 +0800

    sad

---
+++ practice/src/main/java/concurrent/threadunsafe/ThreadUnsafe.java
@@ -0,0 +1,15 @@
+package concurrent.threadunsafe;
+
+import org.junit.Test;
+
+public class ThreadUnsafe {
+
+    public static volatile int _count = 0;
+
+
+    @Test
+    public void test1(){
+
+
+    }
+}
