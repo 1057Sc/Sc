feef62acd2d11daf72af6b52a4150abba02d22fb
zqx <13525233384@163.com>
2020-03-24 17:36:46 +0800

    saads

--- practice/src/main/java/concurrency/threadunsafe/ThreadUnsafe.java
+++ practice/src/main/java/concurrency/threadunsafe/ThreadUnsafe.java
@@ -10,19 +10,29 @@ public class ThreadUnsafe {
     public static volatile int _count = 20;
 
 
-    public void test1() {
-
+    /**
+     * It's not atomic
+     */
+    public void unSafe() {
         while (_count > 0) {
             _count--;
             System.out.println(Thread.currentThread().getName() + ":::::::" + _count);
         }
+    }
 
+    public void safe() {
+        synchronized (this) {
+            while (_count > 0) {
+                _count--;
+                System.out.println(Thread.currentThread().getName() + ":::::::" + _count);
+            }
+        }
     }
 
     @Test
     public void run1() {
 
-        Runnable task1 = () -> test1();
+        Runnable task1 = () -> unSafe();
 
         Thread thread1 = new Thread(task1);
 
