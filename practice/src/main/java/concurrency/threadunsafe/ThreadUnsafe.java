e7632011098348b9ab6ed1ab5ea44b8bfac7f281
zqx <13525233384@163.com>
2020-03-25 08:21:21 +0800

    optimize annotation

--- practice/src/main/java/concurrency/threadunsafe/ThreadUnsafe.java
+++ practice/src/main/java/concurrency/threadunsafe/ThreadUnsafe.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class ThreadUnsafe {
 
     /**
-     * It's not thread safe to use volatile or not
+     * volatile is thread safe but not atomic
      */
     public static volatile int _count = 20;
 
@@ -15,7 +15,7 @@ public class ThreadUnsafe {
      */
     public void unSafe() {
         while (_count > 0) {
-            _count--;
+            --_count;
             System.out.println(Thread.currentThread().getName() + ":::::::" + _count);
         }
     }
@@ -32,7 +32,7 @@ public class ThreadUnsafe {
     @Test
     public void run1() {
 
-        Runnable task1 = () -> unSafe();
+        Runnable task1 = () -> safe();
 
         Thread thread1 = new Thread(task1);
 
