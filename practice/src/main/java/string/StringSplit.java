0c471353a92a1a701d60ae40cffcad325e8dc4ec
zqx <13525233384@163.com>
2020-06-11 16:56:53 +0800

    practice daily

--- practice/src/main/java/string/StringSplit.java
+++ practice/src/main/java/string/StringSplit.java
@@ -1,13 +1,17 @@
 package string;
 
 import com.sun.xml.internal.ws.server.ServerRtException;
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * Created by Sc on 5/9/2019.
  */
 public class StringSplit {
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         Demo1();
     }
 
@@ -17,5 +21,27 @@ public class StringSplit {
         String[] arr = a.split(",");
         String[] brr = b.split(",");
         System.out.println(arr);
+    }*/
+
+    @Test
+    public void demo2() {
+        String s = "|";
+
+        String s1 = "\\|";
+
+        int i = 5;
+
+        String s2 = "";
+
+        for (int i1 = 0; i1 < i; i1++) {
+            s2 = s2 + i1 + s;
+        }
+
+        System.out.println(s2);
+
+        String[] split = StringUtils.split(s2, s1);
+
+        System.out.println(Arrays.toString(split));
+
     }
 }
