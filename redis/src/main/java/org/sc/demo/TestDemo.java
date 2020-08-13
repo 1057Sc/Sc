ce05b44614fe29afce194f73e6fb9c13a304d145
zqx <13525233384@163.com>
2020-08-13 18:21:53 +0800

    小数点问题

--- redis/src/main/java/org/sc/demo/TestDemo.java
+++ redis/src/main/java/org/sc/demo/TestDemo.java
@@ -5,9 +5,11 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.Date;
+
 public class TestDemo {
 
-    private static final JedisPool pool = new JedisPool(new JedisPoolConfig(), "192.168.0.9");
+    private static final JedisPool pool = new JedisPool(new JedisPoolConfig(), "127.0.0.1");
 
     @Test
     public void demo1() {
@@ -18,4 +20,23 @@ public class TestDemo {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void demo2() {
+        try (Jedis resource = pool.getResource()) {
+            String zSetKey = "scsc1";
+            long score = 1;
+            String member = "124";
+            Double timeSign = 1 - (new Date().getTime()) / 100000000000000d;
+            Double currentScore = resource.zincrby(zSetKey, score, member);
+            Double dif = timeSign - (currentScore - currentScore.intValue());
+            System.out.println(resource.zincrby(zSetKey, dif, member));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // 1.9840268878862402
+    // 1.98402688767237
 }
