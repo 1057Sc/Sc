22262b160a2c533307455a8660570df1dfe4a3d6
sc <13525233384@163.com>
2020-12-07 14:02:50 +0800

    demos

---
+++ practice/src/main/java/reflect/mthodhandler/demos/ConstantCallSiteDemo.java
@@ -0,0 +1,26 @@
+package reflect.mthodhandler.demos;
+
+import java.lang.invoke.*;
+
+public class ConstantCallSiteDemo {
+
+    private static MethodHandle hw;
+
+    private static void hw() {
+        System.out.println("Hello, codemao!");
+    }
+
+    public static CallSite bootstrapDynamic(MethodHandles.Lookup caller,
+                                            String name,
+                                            MethodType type)
+            throws IllegalAccessException, NoSuchMethodException {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        Class thisClass = lookup.lookupClass();
+        hw = lookup.findStatic(thisClass, "hw", MethodType.methodType(void.class));
+        if (!type.equals(hw.type())) {
+            hw = hw.asType(type);
+        }
+    
+        return new ConstantCallSite(hw);
+    }
+}
