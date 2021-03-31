b5ba94f0774902d9e2b05da39a278b2818e284a2
sc <13525233384@163.com>
2021-03-31 09:56:35 +0800

    12123

---
+++ practice/src/main/java/finalusage/DemoFinally.java
@@ -0,0 +1,31 @@
+package finalusage;
+
+import org.junit.Test;
+
+/**
+ * @author zhuqingxin
+ * @date 2021/3/31
+ */
+public class DemoFinally {
+
+    /**
+     * --try--
+     * --finally--
+     * true
+     */
+    @Test
+    public void demo(){
+        boolean b = demo1();
+        System.out.println(b);
+    }
+
+
+    private boolean demo1() {
+        try {
+            System.out.println("--try--");
+            return true;
+        } finally {
+            System.out.println("--finally--");
+        }
+    }
+}
