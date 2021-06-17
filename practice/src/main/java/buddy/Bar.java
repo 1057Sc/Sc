a0449983c3a93251fc3597e92b1fc67607601ffa
sc <13525233384@163.com>
2021-06-17 18:59:58 +0800

    byte buddy

---
+++ practice/src/main/java/buddy/Bar.java
@@ -0,0 +1,25 @@
+package buddy;
+
+import net.bytebuddy.implementation.bind.annotation.BindingPriority;
+
+/**
+ * @author zhuqingxin
+ * @date 2021/6/17
+ */
+public class Bar {
+
+    @BindingPriority(3)
+    public static String sayHelloBar() {
+        return "Holla in Bar!";
+    }
+
+    @BindingPriority(2)
+    public static String sayBar() {
+        return "bar";
+    }
+
+    public String bar() {
+        return Bar.class.getSimpleName() + " - Bar";
+    }
+
+}
---
+++ practice/src/main/java/buddy/Foo.java
@@ -0,0 +1,13 @@
+package buddy;
+
+/**
+ * @author zhuqingxin
+ * @date 2021/6/17
+ */
+public class Foo {
+
+    public String sayHelloFoo() {
+        return "Hello in Foo!";
+    }
+
+}
