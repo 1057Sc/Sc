7c68db1fa5b8af7465af0442988ebd14cb772d21
sc <13525233384@163.com>
2021-06-17 15:00:21 +0800

    wildcard use case

--- practice/src/main/java/java8/sfuction/SFunctionTest.java
+++ practice/src/main/java/java8/sfuction/SFunctionTest.java
@@ -7,6 +7,8 @@ import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 
 /**
@@ -44,5 +46,37 @@ public class SFunctionTest {
         String implClass = serializedLambda.getImplClass();
         // String implMethodName = serializedLambda.he();
         System.out.println(aClass);
+    }
+
+    @Test
+    public void test3() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        SFunction<String, List<Student>> fun1 = (s) -> Collections.singletonList(new Student());
+        Class<? extends SFunction> aClass = fun1.getClass();
+        Method[] declaredMethods = aClass.getDeclaredMethods();
+        Method method = fun1.getClass().getDeclaredMethod("writeReplace");
+        method.setAccessible(true);
+        SerializedLambda invoke = (SerializedLambda) method.invoke(fun1);
+        String implMethodSignature = invoke.getImplMethodSignature();
+        String functionalInterfaceClass = invoke.getFunctionalInterfaceClass();
+        String functionalInterfaceMethodSignature = invoke.getFunctionalInterfaceMethodSignature();
+        String getter = invoke.getImplMethodName();
+        String implClass = invoke.getImplClass();
+        func(fun1);
+    }
+
+
+    private <R> void func(SFunction<String, List<R>> func) {
+        try {
+            Class<? extends SFunction> aClass = func.getClass();
+            Method[] declaredMethods = aClass.getDeclaredMethods();
+            Method method = func.getClass().getDeclaredMethod("writeReplace");
+            method.setAccessible(true);
+            SerializedLambda invoke = (SerializedLambda) method.invoke(func);
+            String getter = invoke.getImplMethodName();
+            String implClass = invoke.getImplClass();
+            System.out.println(123213);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
