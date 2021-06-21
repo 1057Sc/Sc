31de7fd8e3b7a312462e8bd235d3223133731fdc
sc <13525233384@163.com>
2021-06-21 19:54:05 +0800

    byte buddy

--- practice/src/main/java/buddy/Bar.java
+++ practice/src/main/java/buddy/Bar.java
@@ -8,9 +8,9 @@ import net.bytebuddy.implementation.bind.annotation.BindingPriority;
  */
 public class Bar {
 
-    @BindingPriority(3)
+    // @BindingPriority(3)
     public static String sayHelloBar() {
-        return "Holla in Bar!";
+        return "Hello in Bar!";
     }
 
     @BindingPriority(2)
--- practice/src/main/java/buddy/ByteBuddyDemo.java
+++ practice/src/main/java/buddy/ByteBuddyDemo.java
@@ -23,24 +23,39 @@ public class ByteBuddyDemo {
     @Test
     public void givenObject_whenToString_thenReturnHelloWorldString() throws InstantiationException, IllegalAccessException {
         DynamicType.Unloaded unloadedType = new ByteBuddy()
+                // 定义子类
                 .subclass(Object.class)
+                // 匹配方法
                 .method(ElementMatchers.isToString())
-                .intercept(FixedValue.value("Hello World ByteBuddy!")).make();
-
+                // 拦截
+                .intercept(FixedValue.value("Hello World ByteBuddy!"))
+                // Creates the dynamic type this builder represents.
+                .make();
+        // 双亲委派式加载
         Class<?> dynamicType = unloadedType.load(getClass().getClassLoader()).getLoaded();
+
+        // 输出结果 Hello World ByteBuddy!
+        System.out.println(dynamicType.newInstance().toString());
 
         Assert.assertEquals(dynamicType.newInstance().toString(), "Hello World ByteBuddy!");
     }
 
     @Test
     public void givenFoo_whenRedefined_thenReturnFooRedefined() throws Exception {
+        // 重新定义已经加载过的类 需要使用ByteBuddyAgent
         ByteBuddyAgent.install();
+        // 重新定义类
         new ByteBuddy().redefine(Foo.class)
+                // 匹配方法
                 .method(ElementMatchers.named("sayHelloFoo"))
+                // 拦截
                 .intercept(FixedValue.value("Hello Foo Redefined"))
-                .make().load(Foo.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+                .make()
+                // 加载  ClassReloadingStrategy提供了多种重加载策略
+                .load(Foo.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
         Foo f = new Foo();
         String s = f.sayHelloFoo();
+        // 重新定义后输出：Hello Foo Redefined
         System.out.println(s);
         Assert.assertEquals(f.sayHelloFoo(), "Hello Foo Redefined");
     }
@@ -49,10 +64,16 @@ public class ByteBuddyDemo {
     public void givenSayHelloFoo_whenMethodDelegation_thenSayHelloBar() throws IllegalAccessException, InstantiationException {
 
         String r = new ByteBuddy().subclass(Foo.class)
+                // 匹配方法
                 .method(ElementMatchers.named("sayHelloFoo")
                         .and(ElementMatchers.isDeclaredBy(Foo.class).and(ElementMatchers.returns(String.class)))
                 )
-                .intercept(MethodDelegation.to(Bar.class)).make().load(getClass().getClassLoader()).getLoaded().newInstance()
+                // 委托方法 配合@BindingPriority(2)使用
+                .intercept(MethodDelegation.to(Bar.class))
+                .make()
+                .load(getClass().getClassLoader())
+                .getLoaded()
+                .newInstance()
                 .sayHelloFoo();
 
         System.out.println(r);
@@ -66,15 +87,21 @@ public class ByteBuddyDemo {
         Class<?> type = new ByteBuddy()
                 .subclass(Object.class)
                 .name("MyClassName")
+                // 定义方法
                 .defineMethod("custom", String.class, Modifier.PUBLIC)
+                // 委托方法 配合@BindingPriority(2)使用
                 .intercept(MethodDelegation.to(Bar.class))
+                // 定义字段
                 .defineField("x", String.class, Modifier.PUBLIC).make()
+                // 加载
                 .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER).getLoaded();
 
+        // 用反射的方式访问刚定义的方法
         Method m = type.getDeclaredMethod("custom", null);
 
         System.out.println(m.invoke(type.newInstance()));
         System.out.println(Bar.sayHelloBar());
+        // 用反射的方式访问刚定义的字段
         System.out.println(type.getDeclaredField("x"));
         Assert.assertEquals(m.invoke(type.newInstance()), Bar.sayHelloBar());
         Assert.assertNotNull(type.getDeclaredField("x"));
