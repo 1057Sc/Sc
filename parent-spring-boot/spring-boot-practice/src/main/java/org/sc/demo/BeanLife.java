5d0a829833a0ed5cacedafd2acc186736d600087
sc <13525233384@163.com>
2021-08-13 17:50:02 +0800

    spring bean life

---
+++ parent-spring-boot/spring-boot-practice/src/main/java/org/sc/demo/BeanLife.java
@@ -0,0 +1,51 @@
+package org.sc.demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+
+public class BeanLife implements BeanFactoryPostProcessor,
+        InstantiationAwareBeanPostProcessor,
+        BeanFactoryAware, BeanNameAware,
+        BeanPostProcessor,
+        InitializingBean,
+        DisposableBean {
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String name) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
+}
